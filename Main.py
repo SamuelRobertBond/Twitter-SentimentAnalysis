340997c7bfee46d0489aa29818229634d0d67b87
Samuel Bond <bonds3@go.stockton.edu>
2017-01-23 17:33:35 -0500

    ~Tweets can now be mined based upon a search term  -I need to add some metric in so that I do not get Rate Limited  -Stores collected tweets in database   -Still needs to be tested
    Signed-off-by: Samuel Bond <bonds3@go.stockton.edu>

---
+++ Main.py
@@ -0,0 +1,4 @@
+import TweetRetriever
+
+TweetRetriever.mine_tweets("Programmer")
+
--- TweetRetriever.py
+++ TweetRetriever.py
@@ -1,18 +1,43 @@
 import DatabaseManager
 import tweepy
 
-consumer_key = "c1txbSNlGU5CzquT2lQyY8yLX"
-consumer_secret_key = "SImAc0YrnR1WVp09aU4iRT3X24CGhz8P5okLA8quEKpK7xYgbO"
-access_token = "821903005389824000-Zg0ACXTDvsEGzasC6rJWjueLpwXVQJh"
-access_token_secret = "w6S8YVlGLntzjeQAmqGVsu2uEkm958o5JqVELpAECA1xK"
+# Credit to this solution on Stack Overflow which helped me figure out searching with Tweepy
+# http://stackoverflow.com/questions/22469713/managing-tweepy-api-search
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+def mine_tweets(search_term):
+    consumer_key = "c1txbSNlGU5CzquT2lQyY8yLX"
+    consumer_secret_key = "SImAc0YrnR1WVp09aU4iRT3X24CGhz8P5okLA8quEKpK7xYgbO"
+    access_token = "821903005389824000-Zg0ACXTDvsEGzasC6rJWjueLpwXVQJh"
+    access_token_secret = "w6S8YVlGLntzjeQAmqGVsu2uEkm958o5JqVELpAECA1xK"
 
-public_tweets = api.home_timeline()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
 
-for tweet in public_tweets:
-    print(tweet.text)
+    api = tweepy.API(auth)
+    max_tweets = 1000
+    tweets = []
+    last_id = -1
+
+    while len(tweets) < max_tweets:
+        try:
+            count = max_tweets - len(tweets)
+            new_tweets = api.search(q=search_term, count=count, max_id=str(last_id - 1))
+            if not new_tweets:
+                break
+            tweets.extend(new_tweets)
+            last_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+            print(e.message)
+            break
+
+    for tweet in tweets:
+        DatabaseManager.record_tweet("Search", "Programmer", tweets.text)
+
+    return
+
+
+
+
+
 
