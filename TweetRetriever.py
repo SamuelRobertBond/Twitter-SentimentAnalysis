8b54265f57c922276daa84ce1a7b3cf54bb3ff0e
Samuel Bond <bonds3@go.stockton.edu>
2017-01-22 21:08:54 -0500

    ~Got basic tweet retrieval working  -Uses tweepy python library

--- TweetRetriever.py
+++ TweetRetriever.py
@@ -1,3 +1,18 @@
 import DatabaseManager
-import sys
+import tweepy
+
+consumer_key = "c1txbSNlGU5CzquT2lQyY8yLX"
+consumer_secret_key = "SImAc0YrnR1WVp09aU4iRT3X24CGhz8P5okLA8quEKpK7xYgbO"
+access_token = "821903005389824000-Zg0ACXTDvsEGzasC6rJWjueLpwXVQJh"
+access_token_secret = "w6S8YVlGLntzjeQAmqGVsu2uEkm958o5JqVELpAECA1xK"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
 
